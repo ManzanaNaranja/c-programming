0b6e7379d2afe191b3f5889ef1fb30d5e3e7acad
Person person <AWieniawski@gmail.com>
2020-10-16 22:08:09 -0700

    added string array

--- 2)Arrays.txt/arrays.c
+++ 2)Arrays.txt/arrays.c
@@ -5,6 +5,12 @@ int main() {
     int ages[5];
     int weight[] = {10, 100, 101, 102, 103};
 
+    const char *a[2];
+    a[0] = "blah";
+    a[1] = "hmm";
+
+    printf("first string in arr: %s\n", a[0]);
+
     ages[0] = 11;
     ages[1] = 2;
     ages[2] = 79;
