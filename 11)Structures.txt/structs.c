59ea64fa3f966b77f55dad35003d2b185a95c306
Person person <AWieniawski@gmail.com>
2020-10-18 08:42:27 -0700

    added structs

---
+++ 11)Structures.txt/structs.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+
+struct point {
+    int x;
+    int y;
+};
+
+
+typedef struct{
+    char * brand;
+    int model;
+} vehicle;
+
+
+
+void draw2(vehicle car);
+
+void draw(struct point p);
+
+
+int main() {
+
+    // structures are foundation for objects and classes in c.
+
+    struct point p;
+
+    p.x = 10;
+    p.y = 5;
+
+    draw(p);
+
+    vehicle car;
+    car.brand = "Tesla";
+    car.model = 2020;
+
+    draw2(car);
+
+    return 0;
+}
+
+void draw(struct point p) {
+        printf("X: %d Y: %d\n", p.x, p.y);
+}
+
+void draw2(vehicle car) { // leave out structs since it is a typedef
+        printf("X: %s Y: %d\n", car.brand, car.model);
+}
+
