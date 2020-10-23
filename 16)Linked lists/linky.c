439887876cd6dafa1a1b8707a4f58c2653e46b8c
Person person <AWieniawski@gmail.com>
2020-10-22 19:08:06 -0700

    added linked lists

--- 16)Linked lists/linky.c
+++ 16)Linked lists/linky.c
@@ -9,9 +9,10 @@ void print_list(node_t * head) { // interates over a linked list and printes out
     node_t * current = head;
 
     while (current != NULL) {
-        printf("%d\n", current->val);
+        printf("%d ", current->val);
         current = current->next;
     }
+    printf("\n");
 }
 
 // adds an item to the end of the list
@@ -35,6 +36,8 @@ void addLast(node_t * head, int val) {
     // 2) Link the new item to point to the head of the list
     // 3) Set the head of the list to be our new item
 
+    // explanation of double pointers in funtiion 
+    // https://dev-notes.eu/2018/07/double-pointers-and-linked-list-in-c/
 void addFirst(node_t ** head, int val) {
     node_t * new_node;
     new_node = (node_t *) malloc(sizeof(node_t));
@@ -45,6 +48,95 @@ void addFirst(node_t ** head, int val) {
     *head = new_node;
 }
 
+// Removing the first item (shift)
+
+int shift(node_t ** head) {
+    int retval = -1;
+    node_t * next_node = NULL;
+
+    if(*head == NULL) {
+        return -1;
+    }
+
+    next_node = (*head)->next;
+    retval = (*head)->val;
+    free(*head);
+    *head = next_node;
+
+    return retval; // the value shifted off
+}
+
+// Removing the last item of the list (pop)
+
+int pop(node_t * head) {
+    int retval = 0;
+    // if there is only one item in the list, remove it
+    if (head->next == NULL) {
+        retval = head->val;
+        free(head);
+        return retval;
+    }
+    node_t * current = head;
+    while(current->next->next != NULL) {
+        current = current->next;
+    }
+
+    retval = current->next->val;
+    free(current->next);
+    current->next = NULL;
+    return retval;
+}
+
+// Removes by Index
+int remove_index(node_t ** head, int n) {
+    int i = 0; 
+    int retval = -1;
+    node_t * current = *head;
+    node_t * temp_node = NULL;
+
+    if (n == 0) {
+        return shift(head);
+    }
+
+    for(i = 0; i < n-1; i++) {
+        if (current->next == NULL) {
+            return -1;
+        }
+        current = current->next;
+    }
+    temp_node = current->next;
+    retval = temp_node->val;
+    current->next = temp_node->next;
+    free(temp_node);
+
+    return retval;
+}
+
+// removes by value
+
+int remove_val(node_t ** head, int value) {
+    node_t * current = *head;
+    node_t * temp_node = NULL;
+
+    if(current->val == value) {
+        shift(head);
+        return 1;
+    }
+
+    while(current != NULL) {
+        if(current->next->val == value) {
+            temp_node = current->next;
+            current->next = temp_node->next;
+            free(temp_node);
+            return 1;
+        }
+
+        current = current->next;
+    }
+
+    return 0;
+
+}
 
 
 int main() {
@@ -65,16 +157,41 @@ int main() {
     head->next->next->val = 11;
     head->next->next->next = NULL;
     addLast(head, 3);
-    addLast(head, 10010);
-    addLast(head, 1002222222);
+    addLast(head, 99);
+    addLast(head, 155);
     
-    node_t ** h = &head;
-    addFirst(h, 314);
+    // node_t ** h = &head; // don't actually need to create a double pointer,
+    // just pass in address of the head pointer
+    addFirst(&head, 314);
 
 
 
    // printf("%d", head->val);
    print_list(head);
+   shift(&head);
+   print_list(head);
+   shift(&head);
+   print_list(head);
+   pop(head);
+   print_list(head);
+   remove_index(&head, 3);
+   print_list(head);
+   remove_index(&head, 1);
+   print_list(head);
+   addLast(head, 100);
+   addLast(head, 123);
+   addLast(head, 22);
+   print_list(head);
+   remove_val(&head, 123);
+   print_list(head);
+   remove_index(&head, 2);
+   remove_val(&head, 22);
+   print_list(head);
+
+
+   
+
+   
 
     return 0;
 }
